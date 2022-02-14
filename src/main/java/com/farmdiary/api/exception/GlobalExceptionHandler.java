171463e1b7b8aa93fb86e228eff870e12e22c852
SpearZero <41422767+SpearZero@users.noreply.github.com>
2022-02-14 12:34:58 +0900

    style: 예외에 대한 주석 추가

--- src/main/java/com/farmdiary/api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/farmdiary/api/exception/GlobalExceptionHandler.java
@@ -24,6 +24,7 @@ import java.util.Map;
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
+    // 일반적인 예외(ex, DB에 중복된 값이 존재)
     @ExceptionHandler(DiaryApiException.class)
     public ResponseEntity<ErrorDetails> handleDiaryAPIException(DiaryApiException exception,
                                                                 WebRequest webRequest) {
@@ -38,6 +39,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
 
+    // DB에 값이 존재하지 않으면 발생
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException exception,
                                                                         WebRequest webRequest) {
@@ -51,6 +53,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
 
+    // 토큰 관련 예외
     @ExceptionHandler(TokenException.class)
     public ResponseEntity<ErrorDetails> handleRefreshTokenException(TokenException exception,
                                                                     WebRequest webRequest) {
@@ -64,6 +67,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(errorDetails, HttpStatus.UNAUTHORIZED);
     }
 
+    // 접근 거부시 발생(권한이 없음)
     @ExceptionHandler(AccessDeniedException.class)
     public ResponseEntity<ErrorDetails> handleAccessDeniedException(AccessDeniedException exception,
                                                                     WebRequest webRequest) {
@@ -83,6 +87,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(errorDetails, HttpStatus.FORBIDDEN);
     }
 
+    // 잘못된 username 또는 password 사용시 발생
     @ExceptionHandler(BadCredentialsException.class)
     public ResponseEntity<ErrorDetails> handleBadCredentialsException(BadCredentialsException exception,
                                                                       WebRequest webRequest) {
@@ -96,6 +101,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(errorDetails, HttpStatus.UNAUTHORIZED);
     }
 
+    // 메서드 인자로 설정된 validation이 유효하지 않을 경우 발생
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<ErrorDetails> handleConstraintViolationException(ConstraintViolationException exception,
                                                                            WebRequest webRequest) {
@@ -109,6 +115,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
 
+    // url 경로의 타입과 맞지 않을 경우 발생
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<ErrorDetails> handleMethodArgumentTypeMismatchException(
             MethodArgumentTypeMismatchException exception,
@@ -124,6 +131,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
 
+    // 형식에 맞지 않은 json 전달시 발생
     @Override
     protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
                                                                   HttpHeaders headers,
@@ -142,6 +150,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
 
+    // dto validation예외시 발생
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
                                                                   HttpHeaders headers,
@@ -162,6 +171,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
 
+    // 나머지 예외
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorDetails> handleGlobalException(Exception exception, WebRequest webRequest) {
         Map<String, String> errors = new HashMap<>();
